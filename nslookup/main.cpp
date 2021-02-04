6e2ae2dc30cb88677fbda38a9464731979ac9a85
k0rup710n <61102077+k0rup710n@users.noreply.github.com>
2021-02-04 07:49:41 -0500

    Update main.cpp

--- nslookup/main.cpp
+++ nslookup/main.cpp
@@ -23,7 +23,6 @@ int main(int argc, char *argv[]){
 		printf("getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
 		return 1;
 	}
-	printf("Remote address is: ");
 	char address_buffer[100];
 	char service_buffer[100];
 	getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen, address_buffer, sizeof(address_buffer), service_buffer, sizeof(service_buffer), NI_NUMERICHOST);
